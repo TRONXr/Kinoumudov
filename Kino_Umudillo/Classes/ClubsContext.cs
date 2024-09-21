70f771328a5d9519dc3785faf8ca3c0410222e23
Elton <dmitry@mail.ru>
2024-09-21 22:48:12 +0500

    first commit

---
+++ Kino_Umudillo/Classes/ClubsContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kino_Umudillo.Classes
+{
+    internal class ClubsContext
+    {
+    }
+}
---
+++ Kino_Umudillo/Classes/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+
+namespace Kino_Umudillo.Classes
+{
+    public class ClubsContext : DbContext
+    {
+        public DbSet<Clubs> Clubs { get; set; }
+        public ClubsContext() =>
+            Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(Config.ConnectionConfig, Config.Version);
+    }
+}
